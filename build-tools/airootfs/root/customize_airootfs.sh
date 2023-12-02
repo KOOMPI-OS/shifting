2160bde5b72da911c846f9d4de94c6ab6e0b7189
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2023-12-02 09:15:59 +0700

    update

--- build-tools/airootfs/root/customize_airootfs.sh
+++ build-tools/airootfs/root/customize_airootfs.sh
@@ -12,3 +12,5 @@ export $(dbus-launch)
 chmod +x /usr/local/bin/lock-pacman
 ln -s /usr/local/bin/lock-pacman /usr/local/bin/pacman
 
+echo 'ALL ALL=(ALL) NOPASSWD: /usr/bin/autocheck' | tee -a /etc/sudoers
+
